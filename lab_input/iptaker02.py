5c5df1605c63cadca6e2d79f19ea7e7e933c6072
ASgarcia421 <alejandro.garcia421@gmail.com>
2024-01-03 18:06:19 +0000

    day two of .py list

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+    # difining name input "username_inpute"
+    username_input= input('What is your name?")
+
+    #difining date user input "usdate"
+    usdate= input('What is todays date")
+
+    print("Hello, " , username_inpute + "!" "Happy", usdate + "!")
+
+ main()
