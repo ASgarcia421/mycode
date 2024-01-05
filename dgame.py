88310eb8572c08d499574e7ce6dd38b7f0f9ac7f
ASgarcia421 <alejandro.garcia421@gmail.com>
2024-01-05 17:10:42 +0000

    A bit on error handing

--- dgame.py
+++ dgame.py
@@ -8,11 +8,20 @@ def roll_dice():
 def main():
     print("Welcome to the Dice Rolling Simulator!")
 
+    total = 0
+    num_rolls = 0
+
     while True:
         input("Press Enter to roll the dice...")
 
         result = roll_dice()
         print(f"You rolled a {result}!")
+        
+        total += result
+        num_rolls += 1
+
+        average = total / num_rolls
+        print(f'Averages after {num_rolls} rolls: {average:.2f}')
 
         play_again = input("Do you want to roll again? (yes/no): ").lower()
         if play_again != 'yes':
