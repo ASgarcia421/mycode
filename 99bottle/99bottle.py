0758d5e45a7acd69d81f8e5e5013b4aa340901cb
ASgarcia421 <alejandro.garcia421@gmail.com>
2024-01-05 22:36:44 +0000

    99bottles of bear

---
+++ 99bottle/99bottle.py
@@ -0,0 +1,21 @@
+#!/bin/bash/env python3
+
+
+bottles = 99
+
+while bottles > 0:
+    print(f"{bottles} bottles of beer on the wall, {bottles} bottles of beer.")
+    action = input("Take one down and pass it around, or type 'q' to quit: ")
+    
+    if action.lower() == 'q':
+        break
+    
+    bottles -= 1
+    print(f"{bottles} bottles of beer on the wall.\n")
+
+if bottles == 0:
+    print("No more bottles of beer on the wall, no more bottles of beer.")
+    print("Go to the store and buy some more, 99 bottles of beer on the wall.")
+else:
+    print("Song interrupted. You can continue next time!")
+
